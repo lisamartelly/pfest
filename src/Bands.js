3d83d4a4e72f8a45f813b22756cda75a7fc00c6b
Lisa <lisamichellemurray@gmail.com>
2023-02-28 20:22:16 -0600

    I just started it

--- src/Bands.js
+++ src/Bands.js
@@ -1,8 +1,19 @@
 
 import data from "./data/bands.json";
 import './Bands.css';
+import backupImg from './img/generic_notes.png';
 
-
+const BandTile = ({bandInfo}) => {
+  return <div className="band-tile">
+    <a href={bandInfo.img_url}><img className="tile-image" src={bandInfo.img_url || backupImg}/></a>
+    <div>
+      <h4>{bandInfo.band_name}</h4>
+      <p>Genre: {bandInfo.genre}</p>
+      <p>Location: {bandInfo.porch_address}</p>
+      {/* {bandInfo.info_url} */}
+    </div>
+  </div>
+}
 
 const Bands = () => {
 
@@ -12,6 +23,9 @@ const Bands = () => {
         <h2>
             Check back soon for info on bands!
         </h2>
+        <div className="tiles-container">
+          {data.map(band => <BandTile bandInfo={band}/>)}
+        </div>
       </div>
     </div>
       
--- src/Map.js
+++ src/Map.js
@@ -7,6 +7,11 @@ const Map = () => {
           <h2>
               Coming Soon!
           </h2>
+          <iframe 
+            src="https://www.google.com/maps/d/u/0/embed?mid=1ekLZW7AkqTX2aWN2jIF3Qbz29PX6WNU&ehbc=2E312F" 
+            width="640" 
+            height="480"
+          ></iframe>
         </div>
       </div>
         
