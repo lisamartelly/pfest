d3c163e1ccad3e5704c99737726d0a1adc0efb0f
Lisa <lisamichellemurray@gmail.com>
2023-02-28 20:57:00 -0600

    map page complete. band page functionality exists; needs css

--- src/Bands.js
+++ src/Bands.js
@@ -2,6 +2,7 @@
 import data from "./data/bands.json";
 import './Bands.css';
 import backupImg from './img/generic_notes.png';
+import { useEffect, useState } from "react";
 
 const BandTile = ({bandInfo}) => {
   return <div className="band-tile">
@@ -10,21 +11,54 @@ const BandTile = ({bandInfo}) => {
       <h4>{bandInfo.band_name}</h4>
       <p>Genre: {bandInfo.genre}</p>
       <p>Location: {bandInfo.porch_address}</p>
-      {/* {bandInfo.info_url} */}
     </div>
   </div>
 }
 
+const alphaSort = (band1, band2) => {
+  if (band1.band_name.toLowerCase() < band2.band_name.toLowerCase()) {
+    return -1;
+  }
+  if ((band2.band_name.toLowerCase() < band1.band_name.toLowerCase())) {
+    return 1;
+  }
+  return 0;
+};
+
+
+const sortDict = {
+  'alphabetical': alphaSort,
+  'reverseAlphabetical': (band1, band2) => -1*alphaSort(band1, band2),
+  "genre": (band1, band2) => {
+    if (band1.genre.toLowerCase() < band2.genre.toLowerCase()) {
+      return -1;
+    }
+    if ((band2.genre.toLowerCase() < band1.genre.toLowerCase())) {
+      return 1;
+    }
+    return alphaSort(band1, band2);
+  }
+}
+
 const Bands = () => {
+
+  const [sortBy, setSortBy] = useState('alphabetical');
+  const [sortedData, setSortedData] = useState([]);
+  useEffect(() => {
+    setSortedData([...data].sort(sortDict[sortBy]));
+  }, [sortBy])
 
   return (
     <div className="">
       <div className="bands-container">
-        <h2>
-            Check back soon for info on bands!
-        </h2>
+        <div className="sort-info">
+          <h3>Sort by: </h3>
+          <button onClick={() => setSortBy('alphabetical')}>Alphabetical</button>
+          <button onClick={() => setSortBy('reverseAlphabetical')}>Reverse Alphabetical</button>
+          <button onClick={() => setSortBy('genre')}>Genre</button>
+        </div>
         <div className="tiles-container">
-          {data.map(band => <BandTile bandInfo={band}/>)}
+          {sortedData.map(band => <BandTile bandInfo={band}/>)}
         </div>
       </div>
     </div>
--- src/Map.js
+++ src/Map.js
@@ -3,15 +3,15 @@ const Map = () => {
 
     return (
       <div>
-        <div className="bands-container">
-          <h2>
-              Coming Soon!
-          </h2>
-          <iframe 
-            src="https://www.google.com/maps/d/u/0/embed?mid=1ekLZW7AkqTX2aWN2jIF3Qbz29PX6WNU&ehbc=2E312F" 
-            width="640" 
-            height="480"
-          ></iframe>
+        <div>
+          <div style={{height: '85vh'}}>
+            <iframe 
+              title="porchfest_map"
+              src="https://www.google.com/maps/d/u/0/embed?mid=1ekLZW7AkqTX2aWN2jIF3Qbz29PX6WNU&ehbc=2E312F" 
+              width="100%" 
+              height="100%"
+            ></iframe>
+          </div>
         </div>
       </div>
         
